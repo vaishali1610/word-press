b1cf32634abff1005b39711e63a4d28b477b708a
vaishali1610 <vaishali.balasubramaniam@psiog.com>
2025-09-28 12:51:04 +0530

    added text color and highlight

--- script.js
+++ script.js
@@ -17,8 +17,6 @@ class Editor {
     const words = text.trim().split(/\s+/).filter(Boolean);
     this.wordCount.innerText = words.length;
   }
-
-
   initToolbar() {
     document.querySelectorAll(".tool").forEach(btn => {
       btn.addEventListener("click", () => {
@@ -41,7 +39,30 @@ document.querySelector("[data-action='fontSize']").addEventListener("change", e 
   const size = pxToFontSizeValue(px);  
   document.execCommand("fontSize", false, size);
 });
+document.querySelector("[data-action='heading']").addEventListener("change", e => {
+      const tag = e.target.value; // e.g., h1, h2, p
+      this.execCommand("formatBlock", tag);
+    });
 
+ document.querySelector("[data-meta='font']").addEventListener("change", e => {
+      const font = e.target.value;
+      this.execCommand("fontName", font);
+    });
+
+ const textColorInput = document.createElement("input");
+    textColorInput.type = "color";
+    textColorInput.title = "Text Color";
+    textColorInput.addEventListener("input", e => {
+      this.execCommand("foreColor", e.target.value);
+    });
+    document.querySelector(".toolbar .group:first-child").appendChild(textColorInput);
+    const highlightInput = document.createElement("input");
+    highlightInput.type = "color";
+    highlightInput.title = "Highlight";
+    highlightInput.addEventListener("input", e => {
+      this.execCommand("hiliteColor", e.target.value);
+    });
+    document.querySelector(".toolbar .group:first-child").appendChild(highlightInput);
   }
 }
 
